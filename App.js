8c80638d94559b6cf5e53d51cb4ebec1145b3fb9
X-VOYSAGER <pjsandrade08@gmail.com>
2023-03-29 16:08:39 -0300

    E-livro

--- App.js
+++ App.js
@@ -1,8 +1,34 @@
 import React from "react";
 import TabNavigator from "./components/TabNavigator";
+import { Rajdhani_600SemiBold } from "@expo-google-fonts/rajdhani";
+import * as Font from "expo-font";
 
-export default function App() {
-  return (
-    <TabNavigator/>
-  );
+
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      fontLoaded: false
+    };
+  }
+
+  async loadFonts() {
+    await Font.loadAsync({
+      Rajdhani_600SemiBold: Rajdhani_600SemiBold
+    });
+    this.setState({ fontLoaded: true });
+  }
+
+  componentDidMount() {
+    this.loadFonts();
+  }
+
+  render() {
+    const { fontLoaded } = this.state;
+    if (fontLoaded) {
+      return <TabNavigator />;
+    }
+    return null;
+  }
 }
--- components/TabNavigator.js
+++ components/TabNavigator.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import SearchScreen from "../screens/search";
 import TransactionScreen from "../screens/transaction";
+import Ionicons from "react-native-vector-icons/Ionicons"
 
 const Tab = createBottomTabNavigator() 
 
@@ -10,7 +11,49 @@ export default class TabNavigator extends Component {
     render(){
         return(
             <NavigationContainer>
-                <Tab.Navigator>
+                <Tab.Navigator 
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ focused, color, size }) => {
+                      let iconName;
+          
+                      if (route.name === 'Transaction') {
+                        iconName = focused
+                          ? 'book'
+                          : 'book-outline';
+                      } else if (route.name === 'Search') {
+                        iconName = focused ? 'search' : 'search-outline';
+                      }
+          
+                      // You can return any component that you like here!
+                      return <Ionicons name={iconName} size={size} color={color} />;
+                    }
+                })}
+                tabBarOptions={{
+                  activeTintColor: "#FFFFFF",
+                  inactiveTintColor: "black",
+                  style: {
+                    height: 130,
+                    borderTopWidth: 0,
+                    backgroundColor: "#5653d4"
+                  },
+                  labelStyle: {
+                    fontSize: 20,
+                    fontFamily: "Rajdhani_600SemiBold"
+                  },
+                  labelPosition: "beside-icon",
+                  tabStyle: {
+                    marginTop: 25,
+                    marginLeft: 10,
+                    marginRight: 10,
+                    borderRadius: 30,
+                    borderWidth: 2,
+                    alignItems: "center",
+                    justifyContent: "center",
+                    backgroundColor: "#5653d4"
+                  }
+                }}
+      
+                >
                     <Tab.Screen name="Transaction" component={TransactionScreen} />
                     <Tab.Screen name="Search" component={SearchScreen} />
                 </Tab.Navigator>
