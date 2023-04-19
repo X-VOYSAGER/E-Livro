c3af611fefbb46be0feb5c847de127e01b3454a3
X-VOYSAGER <pjsandrade08@gmail.com>
2023-04-19 16:54:13 -0300

    E-livro versão FINAL

--- App.js
+++ App.js
@@ -2,8 +2,8 @@ import React, { Component } from "react";
 import TabNavigator from "./components/TabNavigator";
 import { Rajdhani_600SemiBold } from "@expo-google-fonts/rajdhani";
 import * as Font from "expo-font";
-
-
+import Login from "./screens/login";
+import { createSwitchNavigator, createAppContainer } from "react-navigation";
 
 export default class App extends Component {
   constructor() {
@@ -27,8 +27,20 @@ export default class App extends Component {
   render() {
     const { fontLoaded } = this.state;
     if (fontLoaded) {
-      return <TabNavigator />;
+      return <AppContainer/>;
     }
     return null;
   }
 }
+const SwitchNavigator = createSwitchNavigator({
+  Login: {
+    screen: Login
+  },
+  BottomTab: {
+    screen: TabNavigator
+  }
+},{
+  initialRouteName: "Login"
+})
+
+const AppContainer =  createAppContainer(SwitchNavigator)
